6b0c8288096d00788cb5a0dbb5a885f483c01288
gmeshaiti <gmeshaiti@gmail.com>
2025-10-02 14:06:04 -0700

    Initial commit - BetterDeal e-commerce store

--- app.py
+++ app.py
@@ -1080,4 +1080,5 @@ def admin_order_detail(order_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=False)
